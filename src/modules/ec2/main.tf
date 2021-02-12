fe6a7f9464bcc413c4f0670dcdc7bb41fad43387
Jagtar Singh <jagtar.singh@siemens.com>
2021-02-13 00:26:05 +0530

    Increased # of ec2 instances from 1 to 4

--- src/modules/ec2/main.tf
+++ src/modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  count                       = var.ec2_should_be_created ? 1 : 0
+  count                       = var.ec2_should_be_created ? 4 : 0
 
   ami                         = var.ec2_ami
   instance_type               = var.ec2_instance_type
